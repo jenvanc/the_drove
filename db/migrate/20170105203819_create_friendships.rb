fbe03cae2bc1a5935d7080aecb1962a6dd44d4a9
Jenna Van Conett & Mark Tanca <pair+jenvanc.mtanca@devbootcamp.com>
2017-01-05 16:54:27 -0500

    Remove question mark from migration

--- db/migrate/20170105203819_create_friendships.rb
+++ db/migrate/20170105203819_create_friendships.rb
@@ -3,7 +3,7 @@ class CreateFriendships < ActiveRecord::Migration[5.0]
     create_table :friendships do |t|
       t.integer :sender_id, foreign_key: true
       t.integer :acceptor_id, foreign_key: true
-      t.boolean :accepted?, default: false
+      t.boolean :accepted, default: false
 
       t.timestamps
     end
