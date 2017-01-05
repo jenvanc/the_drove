e0e2fe92d07e53fa12672880a264074a6429d55d
Jenna Van Conett & Mark Tanca <pair+jenvanc.mtanca@devbootcamp.com>
2017-01-05 16:28:07 -0500

    Create models and migrations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: User
+  belongs_to :receiver, class_name: User
+  validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts, foreign_key: :author_id
+  has_many :receivers, through: :posts
+  has_many :friendships, foreign_key: :sender_id
+  has_many :acceptors, through: :friendships, foreign_key: :acceptor_id
 end
