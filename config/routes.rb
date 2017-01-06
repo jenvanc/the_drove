adc5c4e92496f5a45e96ac18f712b1e7f446b25c
Mark Tanca <email": null>
2017-01-06 12:46:07 -0500

    finished friendships

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :controllers
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :friendships
+  get '/users/index', to: 'users#index'
+
 end
